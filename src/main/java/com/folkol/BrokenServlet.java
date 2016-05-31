5e25b1ba32225eb1a4d746aaa0585823d4d18a94
Matte <mattias.johansson@atex.com>
2016-05-31 13:06:29 +0200

    Fleshed out the example a bit

--- src/main/java/com/folkol/BrokenServlet.java
+++ src/main/java/com/folkol/BrokenServlet.java
@@ -1,18 +1,29 @@
 package com.folkol;
 
 import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-public class BrokenServlet extends javax.servlet.http.HttpServlet {
-    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+public class BrokenServlet extends HttpServlet {
+    private static final Logger LOGGER = Logger.getLogger(BrokenServlet.class.getName());
+    private static final double BROKENNESS = 0.1;
 
-    }
-
-    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
-        if(Math.random() < 0.5) {
-            throw new RuntimeException("Oh noews :(");
-        } else {
-            throw new ServletException("Oh shit :((");
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+        throws ServletException, IOException
+    {
+        if (Math.random() < BROKENNESS) {
+            throw new RuntimeException("Oh noes :(");
         }
+        if (Math.random() < BROKENNESS) {
+            LOGGER.log(Level.SEVERE, "Exceptional!", new Exception("Exceptional!"));
+        }
+        if (Math.random() < BROKENNESS) {
+            LOGGER.severe("Oh crap!");
+        }
+        response.getWriter().println("Hello, world!");
     }
 }
